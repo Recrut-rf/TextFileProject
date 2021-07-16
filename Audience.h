c88fc09f93504e63dcf11c7972772b944adf1268
Recrut-rf <c4wrc@mail.ru>
2021-07-16 22:54:53 +0400

    Update Audience.h
    исправил кодировку

--- Audience.h
+++ Audience.h
@@ -6,14 +6,19 @@
 class Audience
 {
 public:
-	Audience() = default; 
-	
+	Audience() = default;  // конструктор без параметров
+	                       // при создании объекта без параметров
+			       // все его приватные члены будут
+			       // инициализированы дефолтными значениями 
+
 	Audience(std::string _specialization) : specialization(_specialization)
 	{	}
 
 	Audience(int  _roomNumber) : roomNumber(_roomNumber)
 	{	}
-	
+
+	// для редактирования определим методы get (для получения значений)
+	// и set (для установки значений) это есть одна из трех	
 	std::string getSpecialization() const;
 	int getRoomNumber() const;
 	int getFloor() const;
@@ -25,17 +30,27 @@ public:
 	void setFloor(int _floor);
 	void setNumberOfSeats(int _numberOfSeats);
 	void setClassroomArea(int _classroomArea);
-	
-	std::ostream& printAudience(std::ostream& ostr) const; 															
-	std::istream& scanAudience(std::istream& ostr); 
-													  
+
+	// перегрузим операторы ввода вывода для упрощения работы
+	// можно сделать через дружественные функции, но самым правильным способом будет:
+	// оператор вывода
+	std::ostream& printAudience(std::ostream& ostr) const;  // пишем в явном виде метод вывода	
+
+	// оператор ввода
+	std::istream& scanAudience(std::istream& ostr);   // пишем в явном виде метод ввода	
+
+	//---------------------------Перегруженные операторы равенства----------------
 	bool operator== (int number) const;
+
 	bool operator== (std::string specialization) const;
 
-private:	
-	std::string specialization{}; 
-	int roomNumber{};        
-	int floor{};             
-	int numberOfSeats{};     
-	int classroomArea{};     
+private:
+	// все параметры инициализированы дефолтными значениями (для int это 0, для string "")
+	// спомощью {}
+	std::string specialization{}; // специализация
+	int roomNumber{};             // номер аудитории 
+	int floor{};                  // этаж
+	int numberOfSeats{};          // число посадочных мест
+	int classroomArea{};          // площадь аудитории
 };
+
