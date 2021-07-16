fa8a7148590632c5258fe1525c6357bc688c6c22
Recrut-rf <c4wrc@mail.ru>
2021-07-16 20:47:18 +0400

    Update WorkWithAudience.h

--- WorkWithAudience.h
+++ WorkWithAudience.h
@@ -11,30 +11,21 @@ class WorkWithAudience
 {
 public:
 	WorkWithAudience();
-
-	// добавление новой записи
+	
 	void setNewAudience(vector<Audience> &collect);
-
-	// редактирование записи с указанным номером
+	
 	void editingAudience(vector<Audience> &collect, int number);
-
-	// удаление записи с указанным номером
+	
 	void deleteAudience(vector<Audience> &collect, int number);
-
-	// поиск по номеру аудитории
+	
 	void searchByAudienceNumber(const vector<Audience> &collect, int roomNumber);
-
-	// поиск аудитории с заданной специализацией
+	
 	void findingAudienceWithSpecialty(const vector<Audience> &collect, string specialization);
-
-	// Вывод информации на экран: для указанного этажа список аудиторий в порядке возрастания
+	
 	void showAudienceInfo(const vector<Audience> &audienceintsCollection, int floor);
-
-	// запись данных в файл
+	
 	void writeFile(const vector<Audience> &collect, string fileName);
-
-	// чтение данных из файла
+	
 	void readDataFromFile(vector<Audience> &collect, string fileName);
-
 };
 
